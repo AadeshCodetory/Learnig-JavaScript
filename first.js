48e8a192fc3855291df2a1c602f36d3d23a9741a
AadeshCodetory <adeshtiwari869@gmail.com>
2024-03-15 18:47:58 +0530

    js learning

---
+++ first.js
@@ -0,0 +1,15 @@
+//chapter:-1 variables and Data types
+
+
+const profile={
+    pname:'adeshpandit_89',
+    name:"Aadesh Tiwari",
+    posts:132,
+    followers:575,
+    following:120,
+    info:"pursuing B.sc. IT",
+    isFollow:true
+
+
+}
+console.log(profile.followers);
